7fb81adc926de453323a02847c78c51b646a24cf
Fernando Dingler <fdingler@amazon.com>
2021-01-18 12:24:43 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+let i = 0;
+
+function fizzBuzz() {
+    if (i % 2 === 0) {
+        appendListItem("fizz");
+    } else {
+        appendListItem("buzz");
+    }
+
+    i += 1;
+}
+
+function appendListItem(content) {
+    const item = document.createElement("li");
+    item.innerHTML = content;
+
+    const list = document.getElementById("fooBarList");
+    list.appendChild(item);
+}
